9a0079ec375d075cc18672736aa3de7e9bbff061
Carlos E. Caballero B <cijkb.j@gmail.com>
2014-03-12 02:16:10 -0400

    [DIA++]

--- examen/reto3.js
+++ examen/reto3.js
@@ -1,38 +1,65 @@
 var Model=new(function(){
-    this.bombs=10
-    this.rows=20
-    this.columns=20
+    this.bombs=80
+    this.rows=16
+    this.columns=16
 
     var matrix
     this.generateMatrix=function(){
+        // Verificador de numero de bombas
+        if(this.bombs>(this.rows*this.columns)){
+            this.bombs=this.rows*this.columns
+        }
+        // Seteo de matriz
         matrix=new Array(this.rows)
-        remaining=this.bombs
-        count=this.rows*this.columns
         for(i=0;i<this.rows;i++){
             matrix[i]=new Array(this.columns)
             for(j=0;j<this.columns;j++){
-                _bomb=this.random()
-                if(_bomb){
-                    remaining--
+                matrix[i][j]=0
+            }
+        }
+        // Insercion de bombas
+        for(k=0;k<this.bombs;k++){
+            do{
+                _i=this.random(this.rows)
+                _j=this.random(this.columns)
+            }while(matrix[_i][_j]===-1)
+            matrix[_i][_j]=-1
+        }
+        // Insercion de contadores de bombas
+        for(i=0;i<this.rows;i++){
+            for(j=0;j<this.columns;j++){
+                if(matrix[i][j]===0){
+                    matrix[i][j]=1
                 }
-                matrix[i][j]=_bomb
-                count++
             }
         }
     }
-    this.random=function(){
-        return ((Math.floor(Math.random()*2))==0)
+    this.getMatrix=function(){return matrix}
+    this.random=function(max){return Math.floor(Math.random()*max)}
+    this.countBombs=function(i,j,m){
+        // TODO
     }
 })()
 
 var Render=new(function(){
     this.renderTable=function(matrix){
-
+        
     }
-})()
-
-var Tpl=new(function(){
-
+    this.finishGame=function(matrix){
+        tbody=document.querySelector('#field table tbody')
+        for(i=0;i<Model.rows;i++){
+            tr=tbody.children[i]
+            for(j=0;j<Model.columns;j++){
+                td=tr.children[j]
+                if(matrix[i][j]<0){
+                    td.setAttribute('class','bomb')
+                }else{
+                    td.setAttribute('class','n'+matrix[i][j])
+                    td.textContent=matrix[i][j]
+                }
+            }
+        }
+    }
 })()
 
 window.onload=function(){
