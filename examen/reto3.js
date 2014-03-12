529473b043bf8ddbf9463f8373aaf12f173a8de6
Carlos E. Caballero B <cijkb.j@gmail.com>
2014-03-12 14:17:12 -0400

    [RETO3] working on mineswepper

--- examen/reto3.js
+++ examen/reto3.js
@@ -1,5 +1,5 @@
 var Model=new(function(){
-    this.bombs=80
+    this.bombs=64
     this.rows=16
     this.columns=16
 
@@ -26,19 +26,25 @@ var Model=new(function(){
             matrix[_i][_j]=-1
         }
         // Insercion de contadores de bombas
+        h=this.columns
+        w=this.rows
         for(i=0;i<this.rows;i++){
             for(j=0;j<this.columns;j++){
-                if(matrix[i][j]===0){
-                    matrix[i][j]=1
+                if(matrix[i][j]===-1){  // es bomba
+/* sup izq */       (i!=0&&j!=0&&matrix[i-1][j-1]>=0)&&(matrix[i-1][j-1]++);
+/* sup cen */       (i!=0&&matrix[i-1][j]>=0)&&(matrix[i-1][j]++);
+/* sup der */       (i!=0&&matrix[i-1][j+1]>=0)&&(matrix[i-1][j+1]++);
+/* mid izq */       (j!=0&&matrix[i][j-1]>=0)&&(matrix[i][j-1]++);
+/* mid der */       (j!=h-1&&matrix[i][j+1]>=0)&&(matrix[i][j+1]++);
+/* inf izq */       (i!=w-1&&j!=0&&matrix[i+1][j-1]>=0)&&(matrix[i+1][j-1]++);
+/* inf cen */       (i<w-1&&matrix[i+1][j]>=0)&&(matrix[i+1][j]++);
+/* inf der */       (i<w-1&&j<h-1&&matrix[i+1][j+1]>=0)&&(matrix[i+1][j+1]++);
                 }
             }
         }
     }
     this.getMatrix=function(){return matrix}
     this.random=function(max){return Math.floor(Math.random()*max)}
-    this.countBombs=function(i,j,m){
-        // TODO
-    }
 })()
 
 var Render=new(function(){
@@ -54,8 +60,10 @@ var Render=new(function(){
                 if(matrix[i][j]<0){
                     td.setAttribute('class','bomb')
                 }else{
-                    td.setAttribute('class','n'+matrix[i][j])
-                    td.textContent=matrix[i][j]
+                    if(matrix[i][j]!=0){
+                        td.setAttribute('class','n'+matrix[i][j])
+                        td.textContent=matrix[i][j]
+                    }
                 }
             }
         }
